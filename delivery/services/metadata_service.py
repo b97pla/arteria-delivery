a84457859596fab44a630771687144e29ca05cc3
b97pla <pontus.larsson@medsci.uu.se>
2019-04-24 13:52:35 +0200

    handle ws in checksum paths

--- delivery/services/metadata_service.py
+++ delivery/services/metadata_service.py
@@ -32,7 +32,7 @@ class MetadataService(object):
         try:
             with open(checksum_file) as chksumh:
                 for entry in chksumh:
-                    checksum, file_path = entry.split()
+                    checksum, file_path = entry.split(maxsplit=1)
                     file_checksums[file_path] = checksum
         except IOError as e:
             raise ChecksumFileNotFoundException("Checksum file '{}' could not be opened: {}".format(checksum_file, e))
